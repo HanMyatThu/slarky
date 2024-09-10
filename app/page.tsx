798fcdcf3f47ad1d92b10aeee423c96aecd1abb5
Han Myat Thu <hanmyatthu111@gmail.com>
2024-09-10 11:58:36 +0200

    create user button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { useAuthActions } from "@convex-dev/auth/react";
-
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@/components/common/user-button";
 
 const Home = () => {
-  const { signOut } = useAuthActions();
   return (
     <div>
       Hello <span>World</span>
-      <Button onClick={() => signOut()}>Sign out</Button>
+      <UserButton />
     </div>
   );
 };
