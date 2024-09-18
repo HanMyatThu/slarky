fbb38d62cfc0a06f5a8a1d33f1414b01fd5e7c11
Han Myat Thu <hanmyatthu111@gmail.com>
2024-09-18 11:38:49 +0200

    create editor component

--- app/workspace/[workspaceId]/channel/[channelId]/_components/chat-input.tsx
+++ app/workspace/[workspaceId]/channel/[channelId]/_components/chat-input.tsx
@@ -1,9 +1,25 @@
-import Editor from "@/components/common/editor";
+import dynamic from "next/dynamic";
+import Quill from "quill";
+import { useRef } from "react";
 
-export const ChatInput = () => {
+const Editor = dynamic(() => import("@/components/common/editor"), {
+  ssr: false,
+});
+
+interface ChatInputProps {
+  placeholder: string;
+}
+
+export const ChatInput = ({ placeholder }: ChatInputProps) => {
+  const editorRef = useRef<Quill | null>(null);
   return (
     <div className="px-5 w-full pb-4">
-      <Editor />
+      <Editor
+        placeHOlder={placeholder}
+        onSubmit={() => {}}
+        disabled={false}
+        innerRef={editorRef}
+      />
     </div>
   );
 };
--- components/common/editor.tsx
+++ components/common/editor.tsx
@@ -1,12 +1,52 @@
 import Quill, { QuillOptions } from "quill";
+import { Delta, Op } from "quill/core";
 import { PiTextAa } from "react-icons/pi";
-import { useEffect, useRef } from "react";
+import { MdSend } from "react-icons/md";
+import { ImageIcon, Smile } from "lucide-react";
+import { MutableRefObject, useEffect, useLayoutEffect, useRef } from "react";
+
+import { Button } from "@/components/ui/button";
+import { ToolTipHint } from "./tooltip-hint";
 
 import "quill/dist/quill.snow.css";
-import { Button } from "@/components/ui/button";
 
-const Editor = () => {
+type EditorValue = {
+  image: File | null;
+  body: string;
+};
+
+interface EditorProps {
+  onSubmit: ({ image, body }: EditorValue) => void;
+  onCancel?: () => void;
+  placeHOlder?: string;
+  defaultValue?: Delta | Op[];
+  disabled?: boolean;
+  innerRef?: MutableRefObject<Quill | null>;
+  variant?: "create" | "update";
+}
+
+const Editor = ({
+  variant = "create",
+  onSubmit,
+  defaultValue,
+  disabled = false,
+  innerRef,
+  onCancel,
+  placeHOlder = "Write something...",
+}: EditorProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const submitRef = useRef(onSubmit);
+  const placeHolderRef = useRef(placeHOlder);
+  const quillRef = useRef<Quill | null>(null);
+  const defaultValueRef = useRef(defaultValue);
+  const disabledRef = useRef(disabled);
+
+  useLayoutEffect(() => {
+    submitRef.current = onSubmit;
+    placeHolderRef.current = placeHOlder;
+    defaultValueRef.current = defaultValue;
+    disabledRef.current = disabled;
+  });
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -18,6 +58,7 @@ const Editor = () => {
 
     const options: QuillOptions = {
       theme: "snow",
+      placeholder: placeHolderRef.current,
     };
     new Quill(editorContainer, options);
 
@@ -32,15 +73,75 @@ const Editor = () => {
       <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-blue-300 focus-within:shadow-md transition bg-white">
         <div ref={containerRef} className="h-full ql-custom" />
         <div className="flex px-2 pb-2 z-[5]">
-          <Button
-            disabled={false}
-            size="iconSm"
-            variant="ghost"
-            onClick={() => {}}
-          >
-            <PiTextAa />
-          </Button>
+          <ToolTipHint label="Hide formatting">
+            <Button
+              disabled={false}
+              size="iconSm"
+              variant="ghost"
+              onClick={() => {}}
+            >
+              <PiTextAa className="size-4" />
+            </Button>
+          </ToolTipHint>
+          <ToolTipHint label="Emoji">
+            <Button
+              disabled={false}
+              size="iconSm"
+              variant="ghost"
+              onClick={() => {}}
+            >
+              <Smile className="size-4" />
+            </Button>
+          </ToolTipHint>
+          {variant === "create" && (
+            <ToolTipHint label="Image">
+              <Button
+                disabled={false}
+                size="iconSm"
+                variant="ghost"
+                onClick={() => {}}
+              >
+                <ImageIcon className="size-4" />
+              </Button>
+            </ToolTipHint>
+          )}
+          {variant === "update" && (
+            <div className="ml-auto flex items-center gap-x-2">
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => {}}
+                disabled={false}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="slacky"
+                size="sm"
+                onClick={() => {}}
+                disabled={false}
+              >
+                Save
+              </Button>
+            </div>
+          )}
+          {variant === "create" && (
+            <Button
+              disabled={false}
+              size="iconSm"
+              variant="slacky"
+              className="ml-auto"
+              onClick={() => {}}
+            >
+              <MdSend className="size-4" />
+            </Button>
+          )}
         </div>
+      </div>
+      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
+        <p>
+          <strong>Shift + Return</strong> to add a new line
+        </p>
       </div>
     </div>
   );
