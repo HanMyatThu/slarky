7730589c68e64000dc9626316c77db715e4b8c3f
Han Myat Thu <hanmyatthu111@gmail.com>
2024-09-27 12:08:03 +0200

    update search toolbar component

--- app/workspace/[workspaceId]/_components/toolbar.tsx
+++ app/workspace/[workspaceId]/_components/toolbar.tsx
@@ -1,24 +1,79 @@
 "use client";
+import { Info, Search } from "lucide-react";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
 import { useGetWorkSpaceById } from "@/features/workspaces/api/use-get-workspace-id";
 import { useWorkSpaceId } from "@/hooks/use-workspace-id";
-import { Info, Search } from "lucide-react";
+import { useGetChannels } from "@/features/channels/api/use-get-channel";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import Link from "next/link";
 
 export const Toolbar = () => {
+  const [open, setOpen] = useState(false);
   const workspaceId = useWorkSpaceId();
+
   const { data } = useGetWorkSpaceById(workspaceId);
+  const { data: channels } = useGetChannels({ workspaceId });
+  const { data: members } = useGetMembers({ workspaceId });
+
   return (
     <nav className="bg-[#481349] flex items-center justify-between h-10 p-1.5">
       <div className="flex-1"></div>
       <div className="min-w-[280px] max-w-[642px] grow-[2] shrink">
         <Button
+          onClick={() => setOpen(true)}
           size="sm"
           className="bg-accent/25 hover:bg-accent/30 w-full justify-start h-7 px-2"
         >
           <Search className="size-4 text-white mr-2" />
           <span className="text-white text-sm">Search {data?.name}</span>
         </Button>
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Channels">
+              {channels?.map((channel) => (
+                <Link
+                  onClick={() => setOpen(false)}
+                  key={channel._id}
+                  href={`/workspace/${workspaceId}/channel/${channel._id}`}
+                >
+                  <CommandItem className="cursor-pointer">
+                    {channel.name}
+                  </CommandItem>
+                </Link>
+              ))}
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Members">
+              {members?.map((member) => (
+                <Link
+                  onClick={() => setOpen(false)}
+                  key={member._id}
+                  href={`/workspace/${workspaceId}/member/${member._id}`}
+                >
+                  <CommandItem className="cursor-pointer">
+                    {member.user.name}
+                  </CommandItem>
+                </Link>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </CommandDialog>
       </div>
       <div className="ml-auto flex-1 flex items-center justify-end">
         <Button variant="transparent" size="iconSm">
